b4cd00e5531ffb896f6166f6040c852fa54af68a
kashiro aluno <paulo.hsrfin@gmail.com>
2023-09-29 23:54:16 -0300

    Aulas de Java 01
    aqui estao algumas aulas de JAVA onde posso acompanhar o meu proprio crescimento.
    concomitantemente eu tambem quero aprender a utilizar o GIT/GIHUB para me organizar e treinar as mensagens dos commits, pull/push e etc.

---
+++ GradeBook.java
@@ -0,0 +1,11 @@
+//Figura 3.1 do livro
+
+
+public class GradeBook{
+
+        //exibe uma mensagem de boas-vindas para o usuario GradeBook
+    public void displayMessage(){
+
+        System.out.println("Bem vindo ao Livro de Notas.");
+    }
+}
---
+++ GradeBookTeste.java
@@ -0,0 +1,15 @@
+//figura 3.2 do livro
+
+
+public class GradeBookTeste{
+
+    //metodo main inicia a execucao de programa
+    public static void main(String[] args){
+
+        //cria um objeto GradeBook e o atribui a myGradeBook
+        GradeBook myGradeBook = new GradeBook();
+
+        //chama metodo displayMessage de myGradeBook
+        myGradeBook.displayMessage();
+    } //fim da main
+}
