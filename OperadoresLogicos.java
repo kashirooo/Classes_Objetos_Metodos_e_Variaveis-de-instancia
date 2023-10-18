a092fc0442174572d4b9a4b5fcf5baf2fb9388d3
kashiro aluno <paulo.hsrfin@gmail.com>
2023-10-17 22:11:28 -0300

    Create OperadoresLogicos.java
    essa é uma maneira dinâmica de mostrar a tabela verdade de operadores lógicos.

---
+++ OperadoresLogicos.java
@@ -0,0 +1,50 @@
+//FIgura 5.18 do livro
+//Operadores Logicos
+
+public class OperadoresLogicos {
+    
+    public static void main( String[] args )
+     {
+       // cria a tabelaverdade para o operador && (E condicional)
+        System.out.printf( "%s\n%s: %b\n%s: %b\n%s: %b\n%s: %b\n\n",
+           "Conditional AND (&&)", "false && false", ( false && false ),
+           "false && true", ( false && true ), 
+           "true && false", ( true && false ),
+           "true && true", ( true && true ) );
+  
+        // cria a tabelaverdade para o operador || (OU condicional)
+        System.out.printf( "%s\n%s: %b\n%s: %b\n%s: %b\n%s: %b\n\n",
+           "Conditional OR (||)", "false || false", ( false || false ),
+           "false || true", ( false || true ),
+           "true || false", ( true || false ),
+           "true || true", ( true || true ) );
+  
+        // cria a tabelaverdade para o operador & (E lógico booleano)
+        System.out.printf( "%s\n%s: %b\n%s: %b\n%s: %b\n%s: %b\n\n",
+           "Boolean logical AND (&)", "false & false", ( false & false ),
+           "false & true", ( false & true ),
+           "true & false", ( true & false ),
+           "true & true", ( true & true ) );
+  
+        // cria a tabelaverdade para o operador | (OU inclusivo lógico booleano) 
+        System.out.printf( "%s\n%s: %b\n%s: %b\n%s: %b\n%s: %b\n\n",
+           "Boolean logical inclusive OR (|)",
+           "false | false", ( false | false ),
+           "false | true", ( false | true ),
+           "true | false", ( true | false ),
+           "true | true", ( true | true ) );
+  
+        // cria a tabelaverdade para o operador ^ (OU lógico booleano exclusivo)
+        System.out.printf( "%s\n%s: %b\n%s: %b\n%s: %b\n%s: %b\n\n",
+           "Boolean logical exclusive OR (^)", 
+           "false ^ false", ( false ^ false ),
+           "false ^ true", ( false ^ true ),
+           "true ^ false", ( true ^ false ),
+           "true ^ true", ( true ^ true ) );
+  
+        // cria a tabelaverdade para o operador ! (negação lógica)
+        System.out.printf( "%s\n%s: %b\n%s: %b\n", "Logical NOT (!)",
+           "!false", ( !false ), "!true", ( !true ) );
+     } // fim de main
+
+}
